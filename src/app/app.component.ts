5ffbecadd2822d9423e6c72d700772ce58030cda
Sebastian Czarnecki <sebastian.czarnecki@jit.team>
2019-03-20 11:19:53 +0100

    Changes for workshop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MqttService } from 'ngx-mqtt';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private mqttService: MqttService) {
+    this.mqttService.connect();
+  }
   title = 'SocketCubeApp';
+
+  turnOff() {
+    this.mqttService.unsafePublish('gniazdka/salon', '16266309', {qos: 0, retain: true});
+  }
+
+  turnOn() {
+    console.log('Turn On command send');
+    this.mqttService.unsafePublish('gniazdka/salon', '16575493', {qos: 0, retain: true});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+
+import {
+  IMqttMessage,
+  MqttModule,
+  IMqttServiceOptions
+} from 'ngx-mqtt';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: 'mqtt.hackcube.org',
+  port: 9001,
+  connectOnCreate: false,
+  path: '',
+  protocol: 'wss'
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +28,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
